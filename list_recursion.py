6e6113ee6034d4241e8232018d588036372bdac9
Cody Doucette <doucette@bu.edu>
2023-04-24 00:30:38 -0400

    Add my solution

--- list_recursion.py
+++ list_recursion.py
@@ -1,15 +1,38 @@
 class ListRecursion:
     def print_sentence(lst):
-        pass
+        if lst is None or lst == []:
+            return
+        if len(lst) == 1:
+            print(lst[0] + '.')
+            return
+        else:
+            print(lst[0] + ' ', end='')
+            ListRecursion.print_sentence(lst[1:])
 
     def reverse(lst):
-        pass
+        if lst is None or lst == []:
+            return lst
+        rest_reversed = ListRecursion.reverse(lst[1:])
+        return rest_reversed + [lst[0]]
 
     def remove_every_other(lst):
-        pass
+        if lst is None or lst == []:
+            return lst
+        if len(lst) == 1:
+            return lst
+        rest_removed = ListRecursion.remove_every_other(lst[2:])
+        return [lst[0]] + rest_removed
 
     def maximum(lst):
-        pass
+        if lst is None or lst == []:
+            return lst
+        if len(lst) == 1:
+            return lst[0]
+        max_in_rest = ListRecursion.maximum(lst[1:])
+        if lst[0] > max_in_rest:
+            return lst[0]
+        else:
+            return max_in_rest
 
 if __name__ == "__main__":
     ListRecursion.print_sentence(['The', 'quick', 'brown', 'fox'])
